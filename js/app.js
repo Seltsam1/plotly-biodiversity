137ebe5aabeddc73cf4c188dcc353e2b52dc9129
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-04-03 19:49:03 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,18 +9,40 @@ function init() {
         console.log(data);
 
         let namesData = data.names;
-        console.log(namesData)
         namesData.forEach(function(names) {
             selector.append("option")
                 .text(names)
                 .property("value", names)
         });
 
-        
-    });
+        let firstSample = namesData[0];
 
-    
+        createChart(firstSample)
+    });
 };
+
+
+// function to create bar chart
+function createChart(sample) {
+    d3.json("samples.json").then(function(data) {
+        let samples = data.samples;
+        let dataArray = samples.filter(function(data) {
+            return data.id === sample;
+        });
+        let otu_ids = dataArray[0].otu_ids;
+        let otu_labels = dataArray[0].otu_labels;
+        let sample_values = dataArray[0].sample_values;
+
+        console.log(otu_ids);
+        console.log(otu_labels);
+        console.log(sample_values);
+    });
+};
+
+// function to display metadata
+// function createMetaData(sample) {
+// }
+
 
 // call function init
 init();
